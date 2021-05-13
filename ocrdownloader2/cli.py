3e8d84615d4ee66dbcd948ee21afae0aac2963a8
Anthony Porthouse <anthony@porthou.se>
2021-05-13 13:27:30 +0100

    Add black for code formatting

--- ocrdownloader2/cli.py
+++ ocrdownloader2/cli.py
@@ -1,31 +1,25 @@
 import click
-
+from typing import Optional
 from .crawler import get_tracks
 from .downloader import download
 
 
 @click.command()
-@click.argument("from", type=click.IntRange(min=1, clamp=True))
-@click.argument("to", type=click.IntRange(min=1, clamp=True))
-def cli(**kwargs):
+@click.argument("start", type=click.IntRange(min=1, clamp=True))
+@click.argument("end", type=click.IntRange(min=1, clamp=True), required=False)
+def cli(start: int, end: Optional[int]):
     """Parse and handle arguments to run OCR Downloader"""
 
-    click.echo(banner())
+    if end is None:
+        end = start
 
+    click.echo(banner())
     click.echo()
 
-    id_from: int
-    id_to: int
+    click.echo(f"From: {start}")
+    click.echo(f"To: {end}")
 
-    id_from, id_to = kwargs['from'], kwargs['to']
-
-    if id_to is None:
-        id_to = id_from
-
-    click.echo(f"From: {id_from}")
-    click.echo(f"To: {id_to}")
-
-    tracks = get_tracks(id_from, id_to)
+    tracks = get_tracks(start, end)
 
     for track in tracks:
         download(track)
@@ -33,12 +27,15 @@ def cli(**kwargs):
 
 def banner() -> str:
     """Return the banner as a string"""
-    return (r'   ____  __________     ____                      __                __         ' + "\n"
-            r'  / __ \/ ____/ __ \   / __ \____ _      ______  / /___  ____  ____/ /__  _____' + "\n"
-            r' / / / / /   / /_/ /  / / / / __ \ | /| / / __ \/ / __ \/ __ \/ __  / _ \/ ___/' + "\n"
-            r'/ /_/ / /___/ _, _/  / /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  __/ /    ' + "\n"
-            r'\____/\____/_/ |_|  /_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/\___/_/  2.0'
-            )
+    return "\n".join(
+        [
+            r"   ____  __________     ____                      __                __         ",
+            r"  / __ \/ ____/ __ \   / __ \____ _      ______  / /___  ____  ____/ /__  _____",
+            r" / / / / /   / /_/ /  / / / / __ \ | /| / / __ \/ / __ \/ __ \/ __  / _ \/ ___/",
+            r"/ /_/ / /___/ _, _/  / /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  __/ /    ",
+            r"\____/\____/_/ |_|  /_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/\___/_/  2.0",
+        ]
+    )
 
 
 if __name__ == "main":
--- ocrdownloader2/crawler.py
+++ ocrdownloader2/crawler.py
@@ -1,14 +1,10 @@
-from typing import List, Optional
-
 import requests
 import re
+from typing import List, Optional
 from bs4 import BeautifulSoup
-
 from .track import Track
 
-headers = {
-    'User-Agent': 'OCRDownloader/2.0.0'
-}
+headers = {"User-Agent": "OCRDownloader/2.0.0"}
 
 
 def get_tracks(start: int, end: int) -> List[Track]:
@@ -26,6 +22,7 @@ def get_tracks(start: int, end: int) -> List[Track]:
 
 def backoff(func):
     from time import sleep
+
     sleep(2)
 
     def wrapper(*args, **kwargs):
@@ -37,31 +34,35 @@ def backoff(func):
 @backoff
 def get_track(track_id) -> Optional[Track]:
 
-    print(f'Loading Track: {track_id}')
+    print(f"Loading Track: {track_id}")
 
     url = get_url_for_track(track_id)
 
     try:
         with requests.get(url, headers=headers, timeout=2) as page:
             if page.status_code != 200:
-                print('Not valid status code, skipping')
+                print("Not valid status code, skipping")
                 return
 
-            print('Parsing Page')
+            print("Parsing Page")
 
-            soup = BeautifulSoup(page.content, 'html5lib')
-            modal = soup.find(id='modalDownload')
+            soup = BeautifulSoup(page.content, "html5lib")
+            modal = soup.find(id="modalDownload")
 
-            checksum = re.search('MD5 Checksum: ([0-9a-f]{32})', modal.text).group(1)
-            links = list(map(lambda link: link['href'], modal.find_all('a', href=re.compile('\\.mp3$'))))
+            checksum = re.search("MD5 Checksum: ([0-9a-f]{32})", modal.text).group(1)
+            links = list(
+                map(
+                    lambda link: link["href"],
+                    modal.find_all("a", href=re.compile("\\.mp3$")),
+                )
+            )
 
             return Track(id=track_id, checksum=checksum, links=links)
     except requests.exceptions.ConnectTimeout:
-        print(f'Timed out connecting to {url}')
+        print(f"Timed out connecting to {url}")
 
     return
 
 
 def get_url_for_track(track_id) -> str:
-    return 'https://ocremix.org/remix/OCR{0:05d}'.format(track_id)
-
+    return "https://ocremix.org/remix/OCR{0:05d}".format(track_id)
